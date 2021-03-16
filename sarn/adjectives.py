5dd38f962a58a7489eac7975f503a37283d1f043
Niklas Korz <niklas@niklaskorz.de>
2021-03-16 14:44:06 +0100

    Only replace one adjective at a time

--- sarn/adjectives.py
+++ sarn/adjectives.py
@@ -1,7 +1,9 @@
 import nltk
+from nltk.tokenize.treebank import TreebankWordDetokenizer
 from pattern.en import wordnet, comparative, superlative
 
 nltk.download("averaged_perceptron_tagger")
+detokenizer = TreebankWordDetokenizer()
 
 ADJ = "JJ"
 ADJ_CMP = "JJR"
@@ -14,11 +16,12 @@ def opposite_adjectives(word):
     return {syn for a in antonyms for syn in a.synonyms}
 
 
-def replace_adjectives(text):
-    text = text.split()
-    tags = nltk.pos_tag(text)
-    candidates = [[]]
-    for token, tag in tags:
+def replace_sent_adjectives(sent):
+    tokens = nltk.word_tokenize(sent)
+    results = []
+    original = []
+    for token, tag in nltk.pos_tag(tokens):
+        results = [tokens + [token] for tokens in results]
         if tag in [ADJ, ADJ_CMP, ADJ_SUP]:
             opposites = opposite_adjectives(token)
             if tag == ADJ_CMP:
@@ -31,13 +34,19 @@ def replace_adjectives(text):
                     superlative(w) if nltk.pos_tag([w])[0][1] != ADJ_SUP else w
                     for w in opposites
                 }
-            words = opposites
-            words.add(token)
-            candidates = [c + [w] for c in candidates for w in words]
-        else:
-            candidates = [c + [token] for c in candidates]
-    return [" ".join(tokens) for tokens in candidates]
+            results += [original + [w if original else w.capitalize()] for w in opposites]
+        original.append(token)
+            
+    return [detokenizer.detokenize(tokens) for tokens in results]
 
+def replace_adjectives(text):
+    results = []
+    original = []
+    for sent in nltk.sent_tokenize(text):
+        results = [sents + [sent] for sents in results]
+        results += [original + [sent] for sent in replace_sent_adjectives(sent)]
+        original.append(sent)
+    return [" ".join(sents) for sents in results]
 
-for c in replace_adjectives("At least three female commissioners spend time at home."):
+for c in replace_adjectives("At least three tenors will take part in the concert. There are tenors who will take part in the concert."):
     print(c)
