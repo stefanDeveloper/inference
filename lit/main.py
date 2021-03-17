12fc687befdcb530db158ccb6a13cface2f39a98
Stefan Machmeier <stefan-machmeier@outlook.com>
2021-03-17 21:04:11 +0100

    Fix main

--- lit/main.py
+++ lit/main.py
@@ -7,20 +7,20 @@ def main():
         description="Finetuning of a specified model on the specified dataset"
     )
 
-    parser.add_argument("models", nargs="+", help='<Required> Path of models')
-    parser.add_argument("dataset", nargs="+", help='<Required> Path of datasets')
+    parser.add_argument("-m", "--models", nargs="+", help='<Required> Path of models', required=True)
+    parser.add_argument("-d", "--datasets", nargs="+", help='<Required> Path of datasets', required=True)
 
     args = parser.parse_args()
 
     print("Loading datasets", args.datasets)
     datasets = {}
     for i, dataset in enumerate(args.datasets):
-        setattr(datasets, f'dataset_{i}', dataset)
+        datasets[f'dataset_{i}'] = dataset
 
-    print("Loading models", args.datasets)
+    print("Loading models", args.models)
     models = {}
     for i, model in enumerate(args.models):
-        setattr(models, f'model_{i}', model)
+        models[f'model_{i}'] = model
 
     lit_demo = dev_server.Server(models, datasets, port=4321)
     return lit_demo.serve()
