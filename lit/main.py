746a8f6fd8583d3ec6de94a9243fd5fe94463108
Stefan Machmeier <stefan-machmeier@outlook.com>
2021-03-17 20:04:09 +0100

    Finish main file - #4

--- lit/main.py
+++ lit/main.py
@@ -2,35 +2,26 @@ import argparse
 
 import lit_nlp
 
-from sarn.data import load_training_dataset
-
 
 def main(_):
     parser = argparse.ArgumentParser(
         description="Finetuning of a specified model on the specified dataset"
     )
 
-    parser.add_argument("model")
-    parser.add_argument("dataset")
+    parser.add_argument("-m", "--models", nargs="+", help='<Required> Path of models', required=True)
+    parser.add_argument("-d", "--dataset", nargs="+", help='<Required> Path of datasets', required=True)
 
     args = parser.parse_args()
 
-    model_name = args.model
-    #tokenizer = AutoTokenizer.from_pretrained(model_name)
-    #model = AutoModelForSequenceClassification.from_pretrained(model_name)
+    print("Loading datasets", args.datasets)
+    datasets = {}
+    for i, dataset in enumerate(args.datasets):
+        setattr(datasets, f'dataset_{i}', dataset)
 
-    print("Loading dataset", args.dataset)
-    #train_dataset, test_dataset = load_training_dataset(args.dataset, tokenizer)
-
-    #datasets = {
-    #    'mnli_matched': MultiNLIData('/path/to/dev_matched.tsv'),
-    #    'mnli_mismatched': MultiNLIData('/path/to/dev_mismatched.tsv'),
-    #}
-
-    #models = {
-    #    'model_bert': NLIModel('../path/to/model/foo/files'),
-    #    'model_deberta': NLIModel('/path/to/model/bar/files'),
-    #}
+    print("Loading models", args.datasets)
+    models = {}
+    for i, model in enumerate(args.models):
+        setattr(models, f'model_{i}', model)
 
     # TODO Load models by parameters
     lit_demo = lit_nlp.dev_server.Server(models, datasets, port=4321)
