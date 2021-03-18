5152fa1b37404419fb210aa35860d227c8bdc543
Niklas Korz <niklas@niklaskorz.de>
2021-03-18 10:28:42 +0100

    Remove training script

--- scripts/train_deberta.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-#SBATCH --job-name=train_deberta
-#SBATCH -p students
-#SBATCH --gres=gpu:1
-#SBATCH -o train_deberta.txt
-
-source .venv/bin/activate
-python -m sarn.train --output-dir "models/deberta" --log-dir "logs/deberta" "microsoft/deberta-large-mnli" "data/training.csv"
-deactivate
