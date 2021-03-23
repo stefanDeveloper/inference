5e42839677d0c9b85255ad326775ab5e84de3182
Niklas Korz <niklas@niklaskorz.de>
2021-03-23 15:28:50 +0100

    Implement ROC curve, closes #7

--- README.md
+++ README.md
@@ -17,6 +17,8 @@ source .venv/bin/activate
 python -m sarn.train --output-dir models/bart-mq --log-dir logs/bart-mq facebook/bart-large-mnli data/training.csv
 # Inference of two sequences (forwards)
 python -m sarn.classify --model models/bart-mq "All dogs jumped over the fence." "Some small dogs jumped over the fence."
+# ROC curve
+python -m sarn.roc models/bart-mq data/evaluation.csv
 ```
 
 For `--model`, any valid [Huggingface model](https://huggingface.co/transformers/pretrained_models.html) (local or remote) can be specified that has been [finetuned for sequence classification](https://huggingface.co/models?pipeline_tag=text-classification), e.g., [`facebook/bart-large-mnli`](https://huggingface.co/facebook/bart-large-mnli), [`microsoft/deberta-large-mnli`](https://huggingface.co/microsoft/deberta-large-mnli) or a local path like `models/bart-mq`. 
---
+++ sarn/roc.py
@@ -0,0 +1,67 @@
+import argparse
+import re
+from sklearn import metrics
+from transformers import (
+    AutoTokenizer,
+    AutoModelForSequenceClassification,
+)
+import torch
+import numpy as np
+import matplotlib.pyplot as plt
+from .data import load_evaluation_dataset, tokenize
+from .labels import Label
+
+def get_valid_filename(s):
+    s = str(s).strip().replace(" ", "-")
+    return re.sub(r"(?u)[^-\w]", "-", s)
+
+parser = argparse.ArgumentParser(
+    description="Finetuning of a specified model on the specified dataset"
+)
+parser.add_argument("model")
+parser.add_argument("dataset")
+
+
+if __name__ == "__main__":
+    args = parser.parse_args()
+    tokenizer = AutoTokenizer.from_pretrained(args.model)
+    model = AutoModelForSequenceClassification.from_pretrained(args.model)
+
+    dataset = load_evaluation_dataset(args.dataset, tokenizer)
+    dataloader = torch.utils.data.DataLoader(dataset, batch_size=16)
+    probas = []
+    batch_cnt = len(dataloader)
+    for i, batch in enumerate(dataloader):
+        classification_logits = model(**batch).logits
+        probas += torch.softmax(classification_logits, dim=1).tolist()
+        if (i+1) % 10 == 0:
+            print("Batch", i+1, "of", batch_cnt)
+    probas = np.array(probas)
+
+    plt.title("Receiver Operating Characteristic\n" + args.model + ", " + args.dataset)
+
+    cont = probas[:,Label.CONTRADICTION.value]
+    fpr, tpr, thresholds = metrics.roc_curve(dataset.labels, cont, pos_label=Label.CONTRADICTION.value)
+    roc_auc = metrics.auc(fpr, tpr)
+    plt.plot(fpr, tpr, "r", label=f"Contradiction (AUC: {roc_auc:0.2f})")
+
+    neut = probas[:,Label.NEUTRAL.value]
+    fpr, tpr, thresholds = metrics.roc_curve(dataset.labels, neut, pos_label=Label.NEUTRAL.value)
+    roc_auc = metrics.auc(fpr, tpr)
+    plt.plot(fpr, tpr, "g", label=f"Neutral (AUC: {roc_auc:0.2f})")
+
+    ent = probas[:,Label.ENTAILMENT.value]
+    fpr, tpr, thresholds = metrics.roc_curve(dataset.labels, ent, pos_label=Label.ENTAILMENT.value)
+    roc_auc = metrics.auc(fpr, tpr)
+    plt.plot(fpr, tpr, "b", label=f"Entailment (AUC: {roc_auc:0.2f})")
+
+
+    plt.legend(loc="lower right")
+    plt.plot([0, 1], [0, 1], "k--")
+    plt.xlim([0, 1])
+    plt.ylim([0, 1])
+    plt.ylabel("True Positive Rate")
+    plt.xlabel("False Positive Rate")
+    base_name = get_valid_filename(args.model) + "_" + get_valid_filename(args.dataset)
+    plt.savefig(f"diagrams/roc_{base_name}.pdf")
+    plt.savefig(f"diagrams/roc_{base_name}.svg")
